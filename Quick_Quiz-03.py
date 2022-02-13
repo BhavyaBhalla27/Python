94490872704a6ffd79c73c27b90f77965327b253
Bhavya Bhalla <91721455+BhavyaBhalla27@users.noreply.github.com>
2022-02-13 14:11:26 +0530

    Quick Quiz

---
+++ Quick_Quiz-03.py
@@ -0,0 +1,5 @@
+fruits = ['Banana','Apple','Jackfruit','Pear']
+i = 0
+while i < 4:                  # 4 is the length of the string
+    print(fruits[i])
+    i = i + 1
---
+++ Quick_Quiz-04.py
@@ -0,0 +1,6 @@
+def greet(name):
+    p = print("Good day", name)
+    
+name = input("Enter your name:\n")
+greet(name)                        # name will be taken as name entered by user 
+                           
