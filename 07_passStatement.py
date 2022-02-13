d897716a1789f76e7a0a63451725cc79e8a019cf
Bhavya Bhalla <91721455+BhavyaBhalla27@users.noreply.github.com>
2022-02-13 14:06:56 +0530

    Loops

---
+++ 07_passStatement.py
@@ -0,0 +1,3 @@
+l = [1,7,8]
+for i in l:
+    pass             # Instructs to do nothing. Hence shows no error
---
+++ 07_whileloop.py
@@ -0,0 +1,13 @@
+#Example-1
+i = 1
+while i < 10:
+    print("Yes " + str(i))
+    i = i + 1              # Prints Yes 1 to Yes 9 and then moves out of the loop
+
+print("Done")
+# Example-2
+i = 0 
+while i < 5:
+    print("Bhavya")
+    i = i + 1
+    
